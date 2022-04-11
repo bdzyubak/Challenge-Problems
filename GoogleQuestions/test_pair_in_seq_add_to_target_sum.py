b76ff944a0a652945ba1cd416f0ad251033b4d1f
bdzyubak <illan7@gmail.com>
2022-04-11 15:52:05 -0500

    Switch to using pytest.

--- GoogleQuestions/test_pair_in_seq_add_to_target_sum.py
+++ GoogleQuestions/test_pair_in_seq_add_to_target_sum.py
@@ -1,5 +1,6 @@
 
 from pair_in_seq_add_to_target_sum import find_pair_in_seq_naive, find_pair_in_seq_complement, find_pair_in_seq_complement, find_pair_in_seq_complement
+import pytest
 
 # This is a unit test file for pair_in_seq_add_to_target_sum 
 def test_find_pair_in_seq_naive(): 
@@ -14,7 +15,7 @@ def test_find_pair_in_seq_complement_sorted():
     assert_common(find_pair_in_seq_complement)
 
 
-def find_pair_in_seq_iterative(): 
+def test_find_pair_in_seq_iterative(): 
     assert_common(find_pair_in_seq_complement)
 
 
@@ -22,9 +23,7 @@ def assert_common(func):
     assert len(func([1,2,3,9],8)) == 0, 'No matching values'
     assert len(func([1,2,4,4],8)) == 2
 
+
 if __name__ == '__main__': 
-    test_find_pair_in_seq_naive()
-    test_find_pair_in_seq_complement()
-    test_find_pair_in_seq_complement_sorted()
-    find_pair_in_seq_iterative()
+    retcode = pytest.main()
     print('Everything passed.')
